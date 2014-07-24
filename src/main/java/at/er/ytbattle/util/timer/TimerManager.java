433d662123c2c57018d4b3603b3998fc89fdcda7
Rene8888 <rene.hollander@hotmail.de>
2014-07-24 10:49:16 +0200

    added *AllTimers methods to timermanager

--- src/main/java/at/er/ytbattle/util/timer/TimerManager.java
+++ src/main/java/at/er/ytbattle/util/timer/TimerManager.java
@@ -19,6 +19,36 @@ public class TimerManager {
         this.timers.remove(timer);
     }
 
+    public void startAllTimers() {
+        for (Timeable t : this.timers) {
+            t.startTimer();
+        }
+    }
+
+    public void stopAllTimers() {
+        for (Timeable t : this.timers) {
+            t.stopTimer();
+        }
+    }
+
+    public void pauseAllTimers() {
+        for (Timeable t : this.timers) {
+            t.pauseTimer();
+        }
+    }
+
+    public void resumeAllTimers() {
+        for (Timeable t : this.timers) {
+            t.resumeTimer();
+        }
+    }
+
+    public void removeAllTimers() {
+        for (Timeable t : this.timers) {
+            t.removeTimer();
+        }
+    }
+
     enum TimeScale {
         MILISECONDS(1), GAMETICK(50), REDSTONETICK(100), SECOND(1000), MINUTE(60000);
 
