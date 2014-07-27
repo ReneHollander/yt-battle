aaf65f55437f260915faac5ccf8021d0bb80365e
Rene8888 <rene.hollander@hotmail.de>
2014-07-27 14:43:51 +0200

    wierd stuff

--- src/main/java/at/er/ytbattle/util/BattleUtils.java
+++ src/main/java/at/er/ytbattle/util/BattleUtils.java
@@ -21,7 +21,7 @@ import com.google.common.collect.Lists;
 public class BattleUtils {
 
     public static final String SAVE_FILE_NAME = "savegame.xml";
-    
+
     private static ItemStack[] starterChestContents;
 
     private static Scoreboard scoreboardBattle;
