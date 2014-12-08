36f5cc0b7edf6054407b4cf6dc8f742fe0837a11
ReneHollander <rene.hollander@hotmail.de>
2014-12-08 12:36:59 +0100

    added new download link

---
+++ src/main/java/at/er/ytbattle/util/LongWrapper.java
@@ -0,0 +1,26 @@
+package at.er.ytbattle.util;
+
+public class LongWrapper {
+
+	private long number;
+
+	public LongWrapper(long value) {
+		this.set(value);
+	}
+
+	public void set(long value) {
+		this.number = value;
+	}
+
+	public void add(long delta) {
+		number += delta;
+	}
+
+	public void sub(long delta) {
+		number -= delta;
+	}
+
+	public long get() {
+		return this.number;
+	}
+}
